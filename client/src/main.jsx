e12dcb7da0c6e929938a553a0a365b4763c6ec5c
codeotherapist <codeo.therapist995@gmail.com>
2025-06-22 19:04:36 +0530

    project completed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { ClerkProvider } from '@clerk/clerk-react'
+import AppContextProvider from './context/AppContext.jsx'
 
 
 // Import your Publishable Key
@@ -16,7 +17,9 @@ if (!PUBLISHABLE_KEY) {
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <App />
+      <AppContextProvider>
+            <App />
+      </AppContextProvider>
     </ClerkProvider>
   </BrowserRouter>,
 )
